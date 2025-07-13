e64856f8f1c90147fddc3c7ccb879ecd69af7fa9
SRE SANDHYA K <158823828+sresandhya@users.noreply.github.com>
2025-07-13 09:30:03 +0530

    Add files via upload

---
+++ gemini-app.py
@@ -0,0 +1,7 @@
+import getpass
+import os
+import streamlit as st
+from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.output_parsers import StrOutputParser
